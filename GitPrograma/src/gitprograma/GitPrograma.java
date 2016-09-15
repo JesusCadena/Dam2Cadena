acfaec6b0502865fb7f303fb435f6a5524ae4e0f
JesusCadena <j.es.cade28@gmail.com>
2016-09-15 19:29:35 +0200

    Update GitPrograma.java

--- GitPrograma/src/gitprograma/GitPrograma.java
+++ GitPrograma/src/gitprograma/GitPrograma.java
@@ -17,6 +17,7 @@ public class GitPrograma {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
+        System.out.println("Que Tal");
     }
     
 }
